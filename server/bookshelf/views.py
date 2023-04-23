0e4a726af41a9084ddfcac3eab596b367a7ffae0
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-23 14:40:19 +0700

    create serializers

--- server/bookshelf/views.py
+++ server/bookshelf/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from bookshelf.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all().order_by('-date_joined')
+	serializer_class = UserSerializer
+	permission_classes = [permissions.IsAuthenticated]
--- server/server/settings.py
+++ server/server/settings.py
@@ -30,6 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
