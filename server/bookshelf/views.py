7e14a510977e979d1ac2a258a904bde11ee8b938
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-24 22:31:46 +0700

    static files

--- server/bookshelf/views.py
+++ server/bookshelf/views.py
@@ -12,4 +12,4 @@ class UserViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
-	permission_classes = [permissions.IsAuthenticated]
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
