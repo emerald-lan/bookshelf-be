31d2573c38a2476be968f6c211a68be895c36404
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-25 13:21:03 +0700

    add migration

--- server/bookshelf/migrations/0001_initial.py
+++ server/bookshelf/migrations/0001_initial.py
@@ -5,9 +5,9 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
-
     dependencies = [
+        ('bookshelf', '0003_user'),
+
     ]
 
     operations = [
