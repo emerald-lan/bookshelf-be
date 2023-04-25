2b7df9f0dd0b065b103ac507b4803dc67fab54ce
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-25 13:26:23 +0700

    rearrange migrations

--- server/bookshelf/migrations/0003_alter_book_image.py
+++ server/bookshelf/migrations/0003_alter_book_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bookshelf', '0001_initial'),
+        ('bookshelf', '0002_book'),
     ]
 
     operations = [
