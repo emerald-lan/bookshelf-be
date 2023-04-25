f23b304d9cb78cb83e3d5e6a8685c95d90ba4b91
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-25 12:54:23 +0700

    custom user model

--- server/bookshelf/models.py
+++ server/bookshelf/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_superuser = models.BooleanField(default=False)
+    username = models.CharField(max_length=30, unique=True, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)   
+    avatar = models.ImageField(upload_to='images/users/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('id', 'is_superuser', 'username', 'email', 'is_active')
+
+    def __str__(self):
+        return self.username
 
 class Book(models.Model):
     title = models.CharField(max_length=100, unique=True)
