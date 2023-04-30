fba6937a98855c5503d51bebfe293f8f267448ea
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-30 23:50:30 +0700

    install jwt

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 from bookshelf import views
 
 
@@ -13,7 +14,9 @@ router.register(r'books', views.BookViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
