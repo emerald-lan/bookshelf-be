2e49a15ce1e2cd4925637914e8af8cf14590be71
Nguyễn Duy Ngọc <92100963+Dautay2402@users.noreply.github.com>
2023-05-15 20:54:08 +0700

    Add some book

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,13 +28,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Additional settings
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10,
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+# }
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
