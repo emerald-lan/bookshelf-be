a7235cd5d6e700f3be351639e99a53c86932d047
Thanh Lan <emeraldlan1105@gmail.com>
2023-06-09 10:03:19 +0700

    modify user method

--- server/bookshelf/serializers.py
+++ server/bookshelf/serializers.py
@@ -1,24 +1,40 @@
-from rest_framework import serializers
-from .models import User, Book, Order, OrderItem
+from rest_framework import serializers, exceptions
+from .models import *
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.password = validated_data.get('password', instance.password)
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.address = validated_data.get('address', instance.address)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.is_superuser = validated_data.get('is_superuser', instance.is_superuser)
+        instance.save()
+        return instance
+    
     class Meta:
         model = User
-        fields = ['url', 'id', 'is_superuser', 'username', 'email', 'password', 'avatar', 'phone_number', 'address', 'is_active']
+        fields = ['id', 'is_superuser', 'username', 'email', 'password', 'avatar', 'phone_number', 'address', 'is_active', 'first_name', 'last_name']
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
-class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
--- server/bookshelf/views.py
+++ server/bookshelf/views.py
@@ -1,13 +1,89 @@
 from rest_framework import viewsets, permissions
-from bookshelf.serializers import UserSerializer, BookSerializer, OrderSerializer, OrderItemSerializer
-from .models import User, Book, Order, OrderItem
+from .serializers import *
+from .models import *
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Retrieve a user
+    # GET /api/users/{id}/
+    def retrieve(self, request, pk=None):
+        # Admin can retrieve any user
+        if (self.request.user.is_superuser):
+            instance = User.objects.filter(id=pk).first()
+            return Response(self.serializer_class(instance).data,
+                        status=status.HTTP_200_OK)
+        
+        # User can only retrieve their own information
+        elif (self.request.user.id != int(pk)):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        else:
+            instance = self.get_object()
+            return Response(self.serializer_class(instance).data,
+                            status=status.HTTP_200_OK)
+
+    # Create a user or users
+    # POST /api/users/
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Update a user
+    # PUT /api/users/{id}/
+    def update(self, request, pk=None):
+        # Admin can update any user
+        if (self.request.user.is_superuser):
+            instance = User.objects.filter(id=pk).first()
+            serializer = self.serializer_class(instance=instance,
+                                                data=request.data,
+                                                )
+        
+        # User can only update their own information
+        elif (self.request.user.id != int(pk)):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        else:
+            instance = self.get_object()
+            serializer = self.serializer_class(instance=instance,
+                                                data=request.data,
+                                                )
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Delete a user
+    # DELETE /api/users/{id}/
+    def destroy(self, request, pk=None):
+        # Admin can delete any user
+        if (self.request.user.is_superuser):
+            instance = User.objects.filter(id=pk).first()
+        
+        # User can only delete their own information
+        elif (self.request.user.id != int(pk)):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            instance = self.get_object()
+
+        instance.delete()
+        return Response(status=status.HTTP_200_OK)
+
+    def get_object(self):
+        return self.request.user
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
